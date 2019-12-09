f72723a8ff9d2322cb1b0dc7d363635e67d21849
jaser-sys <jz0542681703@gmail.com>
2019-12-09 11:42:12 +0200

    add header file

--- overloadingex/Overloading.h
+++ overloadingex/Overloading.h
@@ -1,1 +1,20 @@
-#pragma once
+#include<iostream>
+#include <string>
+
+using namespace std;
+
+class overloading {
+private:
+	int count;
+	string strarr[10];
+	int intarr[10];
+	double doublearr[10];
+	void sort(int* myarr);
+	void sort(double* myarr);
+	void sort(string* myarr);
+public:
+	int middle(int* myarr);
+	double middle(double* myarr);
+	string middle(string* myarr);
+
+};
